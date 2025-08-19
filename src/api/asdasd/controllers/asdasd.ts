bc0062241b1fbabd9756056592e0ef9edbe8c710
KeithN <sarbence@gmail.com>
2025-08-19 13:21:01 +0200

    piacok

---
+++ src/api/asdasd/controllers/asdasd.ts
@@ -0,0 +1,7 @@
+/**
+ * asdasd controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::asdasd.asdasd');
---
+++ src/api/piacok/controllers/piacok.ts
@@ -0,0 +1,7 @@
+/**
+ * piacok controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::piacok.piacok');
