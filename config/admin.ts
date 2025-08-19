b441bdc889acb3df4b4c5ebcfcb01d65744cef12
KeithN <sarbence@gmail.com>
2025-08-19 15:48:29 +0200

    nowworking!

--- config/admin.ts
+++ config/admin.ts
@@ -17,4 +17,8 @@ export default ({ env }) => ({
     nps: env.bool('FLAG_NPS', true),
     promoteEE: env.bool('FLAG_PROMOTE_EE', true),
   },
+  rateLimit: {
+    interval: 60000,
+    max: 100,
+  },
 });
